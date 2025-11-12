bccd7db35ddcded63bb89456ac0f8c62d8481069
rifat7223 <dgsrifat24@gmail.com>
2025-11-12 20:27:55 +0600

    added delete fuction

--- src/Components/Header/Pages/MyaddJob.jsx
+++ src/Components/Header/Pages/MyaddJob.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../Context/AuthProvider';
+import Swal from 'sweetalert2';
 
 const MyaddJob = () => {
     const {user}=useContext(AuthContext)
@@ -14,6 +15,37 @@ const MyaddJob = () => {
             })
         }
     },[user?.email])
+
+  const  handleDlete=(_id)=>{
+    Swal.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+    fetch(`http://localhost:3000/myallJobs/${_id}`,{
+        method:"DELETE"
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+    })
+
+    Swal.fire({
+      title: "Deleted!",
+      text: "Your file has been deleted.",
+      icon: "success"
+    });
+    const remaningJob=myjob.filter(myjob=>myjob._id!==_id)
+    setJob(remaningJob)
+  }
+});
+       
+    }
     return (
         <div>
             <h1>adde job{
@@ -25,14 +57,12 @@ const MyaddJob = () => {
     <thead>
       <tr>
         <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
+          
         </th>
         <th>Name</th>
         <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
+        <th>Update status</th>
+        <th>status</th>
       </tr>
     </thead>
     <tbody>
@@ -47,24 +77,26 @@ const MyaddJob = () => {
             <div className="avatar">
               <div className="mask mask-squircle h-12 w-12">
                 <img
-                  src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                  src={myjob.coverImage
+}
                   alt="Avatar Tailwind CSS Component" />
               </div>
             </div>
             <div>
-              <div className="font-bold">Hart Hagerty</div>
+              <div className="font-bold">{myjob.postedBy}</div>
               <div className="text-sm opacity-50">United States</div>
             </div>
           </div>
         </td>
         <td>
-          Zemlak, Daniel and Leannon
+          {myjob.category}
+
           <br />
           <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
         </td>
-        <td>Purple</td>
+        <td><div className="badge badge-warning">Update job status</div></td>
         <th>
-          <button className="btn btn-ghost btn-xs">details</button>
+          <button onClick={()=>handleDlete(myjob._id)} className="badge badge-warning">delete job</button>
         </th>
       </tr>)
      }
